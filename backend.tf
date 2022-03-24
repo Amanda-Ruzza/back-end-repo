03f42635e074d152ed9e68d8f2b7da98795c8fbb
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2022-03-23 21:59:46 -0400

    Updated backend with the DynamoDB table lock

--- backend.tf
+++ backend.tf
@@ -1,6 +1,9 @@
+
+
 terraform {
   backend "s3" {
     bucket = "talent-academy-account_id-tfstates-amanda"
     key    = "sprint1/week2/training-terraform/terraform.tfstates"
+    dynamodb_table = "terraform-lock"
   }
 }
