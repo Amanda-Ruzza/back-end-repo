7d8501c32ff6175180e1028c6d240af8bdc8e416
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2022-03-25 11:53:07 -0400

    Trying to make this Terraform work

--- backend.tf
+++ backend.tf
@@ -1,9 +1,7 @@
-
-
 terraform {
   backend "s3" {
-    bucket = "talent-academy-muay-thai4323"
+    bucket = "2talent-academy-muay-thai4323"
     key    = "sprint1/week2/training-terraform/terraform.tfstates"
-    dynamodb_table = "terraform-lock"
+    
   }
 }
--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "aws_s3_bucket" "my_project_bucket" {
-    bucket = "talent-academy-muay-thai4323"
+    bucket = "2talent-academy-muay-thai4323"
 
     lifecycle {
       prevent_destroy = true
     }
 
     tags = {
-        Name = "talent-academy-muay-thai4323"
-        Environment = "Test"
+        Name = "2talent-academy-muay-thai4323"
+        Environment = "Talent-Academy"
     }
 }
 
